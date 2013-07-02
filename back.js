0e27d788649bd478b7db28cb3f63cf0ba5cd55db
Arif Kalaycı <arifkalayci@gmail.com>
2013-07-02 23:31:03 +0300

    "Up" and "Reset" options added.

--- back.js
+++ back.js
@@ -1,12 +1,68 @@
-function onClick(info, tab) {
-	chrome.tabs.getSelected(null, function(tab) {
-		chrome.tabs.sendMessage(tab.id, {});
-	});
-}
+var croppedTabs = [];
 
-chrome.contextMenus.create({
-	"title": "Crop Element", 
-	"contexts": ["all"], 
-	"onclick": onClick
+var mainMenuItemId = chrome.contextMenus.create({
+	"title": "Crop Tools", 
+	"contexts": ["all"]
 });
 
+chrome.contextMenus.create({
+	"parentId": mainMenuItemId,
+	"title": "Crop", 
+	"contexts": ["all"],
+	"onclick": function(info, tab) {
+		croppedTabs.push(tab.id);
+		chrome.tabs.sendMessage(tab.id, {action: "crop"});
+		
+		chrome.contextMenus.update(upMenuItemId, {
+			"enabled": true,
+		});
+		
+		chrome.contextMenus.update(resetMenuItemId, {
+			"enabled": true,
+		});
+	}
+});
+
+var upMenuItemId = chrome.contextMenus.create({
+	"parentId": mainMenuItemId,
+	"title": "Up", 
+	"contexts": ["all"],
+	"enabled": false,
+	"onclick": function(info, tab) {
+		chrome.tabs.sendMessage(tab.id, {action: "up"}, function(response) {
+			croppedTabs.splice(croppedTabs.indexOf(tab.id), 1);
+			chrome.contextMenus.update(upMenuItemId, {
+				"enabled": false
+			});
+			chrome.contextMenus.update(resetMenuItemId, {
+				"enabled": false
+			});			
+		});	
+	}
+});
+
+var resetMenuItemId = chrome.contextMenus.create({
+	"parentId": mainMenuItemId,
+	"title": "Reset", 
+	"contexts": ["all"],
+	"enabled": false,
+	"onclick": function(info, tab) {
+		chrome.tabs.sendMessage(tab.id, {action: "reset"});	
+		chrome.contextMenus.update(upMenuItemId, {
+			"enabled": false
+		});
+		chrome.contextMenus.update(resetMenuItemId, {
+			"enabled": false
+		});
+	}
+});
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+	var enable = croppedTabs.indexOf(activeInfo.tabId) !== -1;
+	chrome.contextMenus.update(upMenuItemId, {
+		"enabled": enable
+	});
+	chrome.contextMenus.update(resetMenuItemId, {
+		"enabled": enable
+	});
+});
--- crop.js
+++ crop.js
@@ -1,13 +1,61 @@
-var clickedElement;
+var origBody;
+var clickedElem;
+var currElem;
 
 document.addEventListener("mouseup", function(event){
     if (event.which === 3)
-		clickedElement = event.target;
+		clickedElem = event.target;
 });
+
+function clearBody() {
+	while (document.body.hasChildNodes()) {
+		document.body.removeChild(document.body.lastChild);
+	}
+}
+
+var isFound;
+var foundElem;
+var parentElem;
+
+function search(elem, searched) {
+	if (elem.isEqualNode(searched)) {
+		foundElem = searched;
+		isFound = true;
+	}
+	else if (!isFound) {
+		for (var i = 0; i < elem.children.length; i++) {
+			parentElem = elem;
+			search(elem.children[i], searched);
+			if (isFound) return;
+		}
+	}
+}
 
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
-		document.body.innerHTML = "";
-		document.body.appendChild(clickedElement);
+		if (request.action === "crop") {
+			origBody = document.body.cloneNode(true);
+			clearBody();
+			document.body.appendChild(clickedElem.cloneNode(true));
+			currElem = clickedElem;
+		}
+		else if (request.action === "up") {
+			//document.body.innerHTML = "";
+			isFound = false;
+			//search(origBody, document.body.children[0]);
+			search(origBody, currElem);
+			clearBody();
+			if (parentElem.isEqualNode(origBody)) { 
+				document.body = origBody.cloneNode(true);
+				sendResponse({action: "reset"});
+			}
+			else 
+				document.body.appendChild(parentElem.cloneNode(true));
+				
+			currElem = parentElem;
+		}
+		else if (request.action === "reset") {
+			document.body = origBody.cloneNode(true);
+		}
 	}
 );
